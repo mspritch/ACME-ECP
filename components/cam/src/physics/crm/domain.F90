c1ca15d782422610e328469b5bf76dd8950966e1
Liran Peng <42455273+liranpeng@users.noreply.github.com>
2020-07-29 23:12:51 -0700

    Update domain.F90

--- components/cam/src/physics/crm/domain.F90
+++ components/cam/src/physics/crm/domain.F90
@@ -1,17 +1,20 @@
 ! Set the domain dimensionality, size and number of subdomains.
 
 module domain
-
+  use advection, only: NADV, NADVS
   use crmdims
   implicit none
 
-  integer, parameter :: YES3D = YES3DVAL  ! Domain dimensionality: 1 - 3D, 0 - 2D
-  integer, parameter :: nx_gl = crm_nx ! Number of grid points in X
-  integer, parameter :: ny_gl = crm_ny ! Number of grid points in Y
-  integer, parameter :: nz_gl = crm_nz ! Number of pressure (scalar) levels
+  integer :: YES3D = -1  ! Domain dimensionality: 1 - 3D, 0 - 2D
+  integer :: nx_gl = -1 ! Number of grid points in X
+  integer :: ny_gl = -1 ! Number of grid points in Y
+  integer :: nz_gl = -1 ! Number of pressure (scalar) levels
+
+  real(crm_rknd) :: dx = -1.  ! grid spacing in x direction
+  real(crm_rknd) :: dy = -1.   ! grid spacing in y direction
+
   integer, parameter :: nsubdomains_x  = 1 ! No of subdomains in x
   integer, parameter :: nsubdomains_y  = 1 ! No of subdomains in y
-
 
   ! define # of points in x and y direction to average for
   !   output relating to statistical moments.
@@ -20,7 +23,7 @@ module domain
   ! See Changes_log/README.UUmods for more details.
   integer, parameter :: navgmom_x = -1
   integer, parameter :: navgmom_y = -1
-
+ 
   integer, parameter :: ntracers = 0 ! number of transported tracers (dotracers=.true.)
 
   ! Note:
@@ -29,5 +32,16 @@ module domain
   !  * nsubdomains_x*nsubdomains_y = total number of processors
   !  * if one processor is used, than  nsubdomains_x = nsubdomains_y = 1;
   !  * if ntracers is > 0, don't forget to set dotracers to .true. in namelist
+
+contains
+
+  
+
+  subroutine setup_domain_xy(dx_gl_in, dy_gl_in)
+    real(crm_rknd), intent(in) :: dx_gl_in, dy_gl_in
+    real(crm_rknd) :: dx,dy
+       dx = dx_gl_in
+       dy = dy_gl_in
+  end subroutine setup_domain_xy
 
 end module domain
