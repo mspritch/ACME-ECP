611f59842670520c8e5a2d91cb0bd5218368d459
“Liran <liranp@uci.edu>
2020-08-01 23:11:45 -0700

    allow values of these grid-related parameters to be set after call of crm().

--- components/cam/src/physics/crm/grid.F90
+++ components/cam/src/physics/crm/grid.F90
@@ -9,43 +9,55 @@ module grid
   character(6), parameter :: version = '6.10.4'
   character(8), parameter :: version_date = 'Feb 2013'
 
-  integer, parameter :: nx = nx_gl/nsubdomains_x
-  integer, parameter :: ny = ny_gl/nsubdomains_y
-  integer, parameter :: nz = nz_gl+1        ! note that nz_gl = crm_nz
-  integer, parameter :: nzm = nz-1          ! note that nzm   = crm_nz
+  !bloss/UPCAM 2020-06
+  ! allow values of these grid-related parameters to be set after call of crm().
+  integer :: nx = -1 ! = nx_gl/nsubdomains_x
+  integer :: ny = -1 ! = ny_gl/nsubdomains_y
+  integer :: nz = -1 ! = nz_gl+1        ! note that nz_gl = crm_nz
+  integer :: nzm = -1 ! = nz-1          ! note that nzm   = crm_nz
 
-  integer, parameter :: nsubdomains = nsubdomains_x * nsubdomains_y
+  integer :: test_out = 0
 
-  logical, parameter :: RUN3D = ny_gl.gt.1
-  logical, parameter :: RUN2D = .not.RUN3D
+  integer :: nsubdomains = nsubdomains_x * nsubdomains_y
 
-  integer, parameter :: nxp1 = nx + 1
-  integer, parameter :: nyp1 = ny + 1 * YES3D
-  integer, parameter :: nxp2 = nx + 2
-  integer, parameter :: nyp2 = ny + 2 * YES3D
-  integer, parameter :: nxp3 = nx + 3
-  integer, parameter :: nyp3 = ny + 3 * YES3D
-  integer, parameter :: nxp4 = nx + 4
-  integer, parameter :: nyp4 = ny + 4 * YES3D
+  logical :: RUN3D = -1 ! = ny_gl.gt.1
+  logical :: RUN2D = -1 ! = .not.RUN3D
 
-  integer, parameter :: dimx1_u = -1                !!-1        -1        -1        -1
-  integer, parameter :: dimx2_u = nxp3              !!nxp3      nxp3      nxp3      nxp3
-  integer, parameter :: dimy1_u = 1-(2+NADV)*YES3D  !!1-5*YES3D 1-4*YES3D 1-3*YES3D 1-2*YES3D
-  integer, parameter :: dimy2_u = nyp2+NADV         !!nyp5      nyp4      nyp3      nyp2
-  integer, parameter :: dimx1_v = -1-NADV           !!-4        -3        -2        -1
-  integer, parameter :: dimx2_v = nxp2+NADV         !!nxp5      nxp4      nxp3      nxp2
-  integer, parameter :: dimy1_v = 1-2*YES3D         !!1-2*YES3D 1-2*YES3D 1-2*YES3D 1-2*YES3D
-  integer, parameter :: dimy2_v = nyp3              !!nyp3       nyp3      nyp3      nyp3
-  integer, parameter :: dimx1_w = -1-NADV           !!-4        -3        -2        -1
-  integer, parameter :: dimx2_w = nxp2+NADV         !!nxp5      nxp4      nxp3      nxp2
-  integer, parameter :: dimy1_w = 1-(2+NADV)*YES3D  !!1-5*YES3D 1-4*YES3D 1-3*YES3D 1-2*YES3D
-  integer, parameter :: dimy2_w = nyp2+NADV         !!nyp5      nyp4      nyp3      nyp2
-  integer, parameter :: dimx1_s = -2-NADVS          !!-4        -3        -2        -2
-  integer, parameter :: dimx2_s = nxp3+NADVS        !!nxp5      nxp4      nxp3      nxp3
-  integer, parameter :: dimy1_s = 1-(3+NADVS)*YES3D !!1-5*YES3D 1-4*YES3D 1-3*YES3D 1-3*YES3D
-  integer, parameter :: dimy2_s = nyp3+NADVS        !!nyp5      nyp4      nyp3      nyp3
+  integer :: nxp1 = -1 ! = nx + 1
+  integer :: nyp1 = -1 ! = ny + 1 * YES3D
+  integer :: nxp2 = -1 ! = nx + 2
+  integer :: nyp2 = -1 ! = ny + 2 * YES3D
+  integer :: nxp3 = -1 ! = nx + 3
+  integer :: nyp3 = -1 ! = ny + 3 * YES3D
+  integer :: nxp4 = -1 ! = nx + 4
+  integer :: nyp4 = -1 ! = ny + 4 * YES3D
+  integer :: nx2 = -1
+  integer :: ny2 = -1
+  integer :: n3i = -1
+  integer :: n3j = -1
 
-  integer, parameter :: ncols = nx*ny
+  integer :: dimx1_u = -1 ! = -1                !!-1        -1        -1        -1
+  integer :: dimx2_u = -1 ! = nxp3              !!nxp3      nxp3      nxp3      nxp3
+  integer :: dimy1_u = -1 ! = 1-(2+NADV)*YES3D  !!1-5*YES3D 1-4*YES3D 1-3*YES3D 1-2*YES3D
+  integer :: dimy2_u = -1 ! = nyp2+NADV         !!nyp5      nyp4      nyp3      nyp2
+  integer :: dimx1_v = -1 ! = -1-NADV           !!-4        -3        -2        -1
+  integer :: dimx2_v = -1 ! = nxp2+NADV         !!nxp5      nxp4      nxp3      nxp2
+  integer :: dimy1_v = -1 ! = 1-2*YES3D         !!1-2*YES3D 1-2*YES3D 1-2*YES3D 1-2*YES3D
+  integer :: dimy2_v = -1 ! = nyp3              !!nyp3       nyp3      nyp3      nyp3
+  integer :: dimx1_w = -1 ! = -1-NADV           !!-4        -3        -2        -1
+  integer :: dimx2_w = -1 ! = nxp2+NADV         !!nxp5      nxp4      nxp3      nxp2
+  integer :: dimy1_w = -1 ! = 1-(2+NADV)*YES3D  !!1-5*YES3D 1-4*YES3D 1-3*YES3D 1-2*YES3D
+  integer :: dimy2_w = -1 ! = nyp2+NADV         !!nyp5      nyp4      nyp3      nyp2
+  integer :: dimx1_s = -1 ! = -2-NADVS          !!-4        -3        -2        -2
+  integer :: dimx2_s = -1 ! = nxp3+NADVS        !!nxp5      nxp4      nxp3      nxp3
+  integer :: dimy1_s = -1 ! = 1-(3+NADVS)*YES3D !!1-5*YES3D 1-4*YES3D 1-3*YES3D 1-3*YES3D
+  integer :: dimy2_s = -1 ! = nyp3+NADVS        !!nyp5      nyp4      nyp3      nyp3
+  integer :: dimx1_d = -1
+  integer :: dimx2_d = -1
+  integer :: dimy1_d = -1
+  integer :: dimy2_d = -1
+
+  integer :: ncols = -1 ! = nx*ny
   integer, parameter :: nadams = 3
 
   ! Vertical grid parameters:
@@ -165,19 +177,89 @@ module grid
 
 contains
 
+  subroutine setup_grid(nx_gl_in, ny_gl_in, nz_gl_in)
+       integer, intent(in) :: nx_gl_in, ny_gl_in, nz_gl_in
 
+       RUN3D = ny_gl_in.gt.1
+       RUN2D = .not.RUN3D
+       test_out = 99999.
+       nx_gl = nx_gl_in
+       ny_gl = ny_gl_in
+       nz_gl = nz_gl_in
 
-  subroutine allocate_grid(ncrms)
+       YES3D = 0
+       if (ny_gl.gt.1) YES3D = 1
+
+       nx = nx_gl/nsubdomains_x
+       ny = ny_gl/nsubdomains_y
+       nz = nz_gl+1        ! note that nz_gl = crm_nz
+       nzm = nz-1          ! note that nzm   = crm_nz
+
+       nsubdomains = nsubdomains_x * nsubdomains_y
+
+       RUN3D = .false.
+       if(ny_gl.gt.1) RUN3D = .true.
+       RUN2D = .not.RUN3D
+
+       nxp1 = nx + 1
+       nyp1 = ny + 1 * YES3D
+       nxp2 = nx + 2
+       nyp2 = ny + 2 * YES3D
+       nxp3 = nx + 3
+       nyp3 = ny + 3 * YES3D
+       nxp4 = nx + 4
+       nyp4 = ny + 4 * YES3D
+
+       nx2=nx_gl+2
+       ny2=ny_gl+2*YES3D
+       n3i=3*nx_gl/2+1
+       n3j=3*ny_gl/2+1
+
+       dimx1_u = -1                !!-1        -1        -1        -1
+       dimx2_u = nxp3              !!nxp3      nxp3      nxp3      nxp3
+       dimy1_u = 1-(2+NADV)*YES3D  !!1-5*YES3D 1-4*YES3D 1-3*YES3D 1-2*YES3D
+       dimy2_u = nyp2+NADV         !!nyp5      nyp4      nyp3      nyp2
+       dimx1_v = -1-NADV           !!-4        -3        -2        -1
+       dimx2_v = nxp2+NADV         !!nxp5      nxp4      nxp3      nxp2
+       dimy1_v = 1-2*YES3D         !!1-2*YES3D 1-2*YES3D 1-2*YES3D 1-2*YES3D
+       dimy2_v = nyp3              !!nyp3       nyp3      nyp3      nyp3
+       dimx1_w = -1-NADV           !!-4        -3        -2        -1
+       dimx2_w = nxp2+NADV         !!nxp5      nxp4      nxp3      nxp2
+       dimy1_w = 1-(2+NADV)*YES3D  !!1-5*YES3D 1-4*YES3D 1-3*YES3D 1-2*YES3D
+       dimy2_w = nyp2+NADV         !!nyp5      nyp4      nyp3      nyp2
+       dimx1_s = -2-NADVS          !!-4        -3        -2        -2
+       dimx2_s = nxp3+NADVS        !!nxp5      nxp4      nxp3      nxp3
+       dimy1_s = 1-(3+NADVS)*YES3D !!1-5*YES3D 1-4*YES3D 1-3*YES3D 1-3*YES3D
+       dimy2_s = nyp3+NADVS        !!nyp5      nyp4      nyp3      nyp3
+     
+       dimx1_d=0
+       dimx2_d=nxp1
+       dimy1_d=1-YES3D
+       dimy2_d=nyp1
+
+       ncols = nx*ny
+  end subroutine setup_grid
+
+  subroutine allocate_grid(nz,ncrms,z,pres,zi,presi,adz,adzw,dt3,dz,na,nb,bc)
     use openacc_utils
     implicit none
-    integer, intent(in) :: ncrms
+    integer, intent(in) :: ncrms,nz
+    integer, intent(out) :: na,nb,bc
     real(crm_rknd) :: zero
-
+    real(crm_rknd), allocatable :: z    (:,:)      ! height of the pressure levels above surface,m
+    real(crm_rknd), allocatable :: pres (:,:)  ! pressure,mb at scalar levels
+    real(crm_rknd), allocatable :: zi   (:,:)     ! height of the interface levels
+    real(crm_rknd), allocatable :: presi(:,:)  ! pressure,mb at interface levels
+    real(crm_rknd), allocatable :: adz  (:,:)   ! ratio of the thickness of scalar levels to dz
+    real(crm_rknd), allocatable :: adzw (:,:)  ! ratio of the thinckness of w levels to dz
+    real(crm_rknd), allocatable :: dt3  (:)   ! dynamical timesteps for three most recent time steps
+    real(crm_rknd), allocatable :: dz(:)    ! constant grid spacing in z direction (when dz_constant=.true.)
+    
     allocate( z(ncrms,nz)       )
-    allocate( pres(ncrms,nzm)   )
+    allocate( pres(ncrms,nz-1)   )
     allocate( zi(ncrms,nz)      )
     allocate( presi(ncrms,nz)   )
-    allocate( adz(ncrms,nzm)    )
+    allocate( adz(ncrms,nz-1)    )
     allocate( adzw(ncrms,nz)    )
     allocate( dt3(3)      )
     allocate( dz(ncrms)         )
@@ -221,3 +303,4 @@ contains
 
 
 end module grid
+
