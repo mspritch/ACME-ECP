9863a0ed0662da393d6ece3d2badeb30835045ab
Liran Peng <42455273+liranpeng@users.noreply.github.com>
2020-08-12 17:54:27 -0700

    Revert "change the way to call crm_input%initialize and crm_output_initialize"
    This reverts commit c3f93d3922623fb7d4c44fff79cc7b08ea880cb9.

--- components/cam/src/physics/cam/crm_physics.F90
+++ components/cam/src/physics/cam/crm_physics.F90
@@ -884,13 +884,8 @@ subroutine crm_physics_tend(ztodt, state, tend, ptend, pbuf, cam_in, cam_out,   
    !------------------------------------------------------------------------------------------------
    call crm_state%initialize()
    call crm_rad_initialize(crm_rad)
-   if (ncol .eq. 1) then
-     call crm_input%initialize(1,pver)
-     call crm_output_initialize(crm_output,1,pver)
-   else
-     call crm_input%initialize(pcols,pver)
-     call crm_output_initialize(crm_output,pcols,pver)
-   end if
+   call crm_input%initialize(pcols,pver)
+   call crm_output_initialize(crm_output,pcols,pver)
 
    !------------------------------------------------------------------------------------------------
    ! Set CRM orientation angle
